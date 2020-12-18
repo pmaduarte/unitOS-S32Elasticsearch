d74492a349d3e88b60653375cc83cbc73b4be1bb
root <root@vps520752.ovh.net>
2020-12-18 14:33:21 +0100

    .gitignore fix

--- functionbeat/pkg/pubsub/vendor/go.elastic.co/apm/scripts/jenkins/windows/build-test.bat
+++
@@ -1,9 +0,0 @@
-::
-:: This script runs the build and test
-::
-
-go get -v -u github.com/jstemmer/go-junit-report
-go get -t ./...
-mkdir build
-go test -v ./... 2>&1 | go-junit-report > build\\junit-apm-agent-go.xml
-type build\\junit-apm-agent-go.xml
